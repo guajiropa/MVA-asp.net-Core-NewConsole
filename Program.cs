8321efcad720d6a7ad9a3a78210105526d400863
guajiropa <guajiropa@hotmail.com>
2018-03-01 21:28:42 -0500

    Initil commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NewConsole
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Hello BITCHES!! State yer name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Happy f#cking Birthday, " + name);
+        }
+    }
+}
